bb77b2ad5d8bb9892611ca4a5794ce8f453389b0
Carolyn Vasisko <carolyn@mothermedia.com>
2018-02-23 22:49:09 -0600

    initial files with question to start game included

---
+++ assets/javascript/app.js
@@ -0,0 +1,33 @@
+// Trivia Game 
+
+// declare var's
+
+readyFreddy = "<p>Ready for Some Fun Facts?</p> <button id='yesready' type='button' class='btn btn-primary'>Yes!</button> <button id='notready' type='button' class='btn btn-primary'>Not yet!</button>" ;
+
+
+// Ask player if ready to start
+$('#question').html(readyFreddy);
+    //get answer 
+   // $('document'). on('keyup', function(){
+
+    
+//});
+//  Player ready...Start game
+
+//  Reveal question and answer choices, start timer 
+
+//  while time is available....
+
+    //  listen for keyup 
+
+    //  is answer right? -- WIN 
+
+    //  if not -- LOSE
+
+// Else:  Time's up & no answer chosen -- LOSE
+
+// display updated score, ask play again?
+
+// restart
+
+
